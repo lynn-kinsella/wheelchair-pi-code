a099e90c0f9e6e81aeac9e815d3a7cd885da9d6a
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2023-03-14 14:14:09 -0400

    realtime debugging

--- motor_controller.py
+++ motor_controller.py
@@ -27,7 +27,7 @@ shared_buffer_full = Event()
 shared_buffer = deque([], 200)
 last_osc_recieved_ts = 0
 
-#tf_model = tf.keras.models.load_model("./model_saved")
+tf_model = tf.keras.models.load_model("./model_saved")
 
 '''
 Pin List - 3/8/23
@@ -120,6 +120,7 @@ def prediction_server():
         else:
             data = np.array([shared_buffer.queue]).transpose(0, 2, 1)
             prediction = np.argmax(tf_model.predict(data, verbose=0)[0])
+            print(prediction)
             BCI_history.appendleft(prediction)
 
             hist_weighted_prediction = max([BCI_history.count(x) for x in [0,1,2]])
@@ -208,10 +209,10 @@ def periodic_update():
 if __name__ == "__main__":
     #pwm_thread = Thread(target=set_PWM)
     osc_thread = Thread(target=osc_server_handler)
-    #input_thread = Thread(target=prediction_server)
+    input_thread = Thread(target=prediction_server)
     #state_thread = Thread(target=periodic_update)
 
     #pwm_thread.start()
     osc_thread.start()
-    #input_thread.start()
+    input_thread.start()
     #state_thread.start()
