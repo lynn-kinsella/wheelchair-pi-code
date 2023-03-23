752f8d045ed8ae033b637095996da652149fb3e5
Declan Kinsella <46036342+d-kinsella@users.noreply.github.com>
2023-03-23 15:38:25 -0400

    Update motor_controller.py

--- motor_controller.py
+++ motor_controller.py
@@ -255,7 +255,7 @@ def update_angle_state(state):
     
     diff = state["target"] - state["current"]
     step = 0
-    if diff > 0:
+    if abs(diff) > 0:
         step = 1
         step = min(abs(diff), step) 
         step *= diff/abs(diff) 
