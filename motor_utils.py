42219c07da231be3588406dd570e3658e5575fb9
Declan Kinsella <46036342+d-kinsella@users.noreply.github.com>
2023-03-23 15:16:34 -0400

    Update motor_utils.py

--- motor_utils.py
+++ motor_utils.py
@@ -5,12 +5,12 @@ def motor_map(angle, speed):
     Convert an angle in % from -100 being fully right and 100 being fully left
     """
     
-    if (angle <= 0 ):
+    if (angle >= 0 ):
         # left_factor = 1 - abs(2*angle/math.pi)
         # right_factor = 1
         left_pwm = speed - angle
         right_pwm = speed
-    if (angle > 0):
+    if (angle < 0):
         right_pwm = speed + angle
         left_pwm = speed
         # left_factor = 1
